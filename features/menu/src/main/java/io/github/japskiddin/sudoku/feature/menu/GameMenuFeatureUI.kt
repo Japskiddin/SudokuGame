b6a9114e38a1c96a9c6c900a0af4dd550b7f7a64
Nikita <japskiddin@gmail.com>
2024-03-21 17:24:32 +0800

    * Working with menu screen

--- features/menu/src/main/java/io/github/japskiddin/sudoku/feature/menu/GameMenuFeatureUI.kt
+++ features/menu/src/main/java/io/github/japskiddin/sudoku/feature/menu/GameMenuFeatureUI.kt
@@ -1,18 +1,23 @@
 package io.github.japskiddin.sudoku.feature.menu
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,32 +32,51 @@ fun MenuScreen() {
 
 @Composable
 internal fun MenuScreen(viewModel: MenuViewModel) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.menu_background),
+                contentScale = ContentScale.Crop
+            )
     ) {
-        Text(text = stringResource(id = R.string.title))
-        Menu(modifier = Modifier.fillMaxWidth(.5f))
-        Text(text = viewModel.getCurrentYear())
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = stringResource(id = R.string.title))
+            Menu(
+                modifier = Modifier
+                    .fillMaxWidth(.5f)
+                    .weight(1f)
+            )
+            Text(text = viewModel.getCurrentYear())
+        }
     }
 }
 
 @Composable
 fun Menu(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier,
+        modifier = modifier.wrapContentHeight(),
+        verticalArrangement = Arrangement.Center,
     ) {
         MenuButton(
             icon = painterResource(id = R.drawable.ic_start_game),
-            text = stringResource(id = R.string.start_game)
+            text = stringResource(id = R.string.start_game),
+            onClick = {}
         )
         MenuButton(
             icon = painterResource(id = R.drawable.ic_start_game),
-            text = stringResource(id = R.string.settings)
+            text = stringResource(id = R.string.settings),
+            onClick = {}
         )
         MenuButton(
             icon = painterResource(id = R.drawable.ic_start_game),
-            text = stringResource(id = R.string.records)
+            text = stringResource(id = R.string.records),
+            onClick = {}
         )
     }
 }
@@ -61,11 +85,13 @@ fun Menu(modifier: Modifier = Modifier) {
 fun MenuButton(
     icon: Painter,
     text: String,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .clickable { onClick() }
             .padding(12.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
@@ -93,6 +119,7 @@ fun MenuButtonPreview() {
     MenuButton(
         icon = painterResource(id = R.drawable.ic_start_game),
         text = stringResource(id = R.string.start_game),
+        onClick = {},
     )
 }
 
