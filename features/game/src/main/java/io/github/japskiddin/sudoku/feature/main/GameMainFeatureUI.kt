e1347891b956736459059e8e07f5eef03f3ef1fd
Nikita <japskiddin@gmail.com>
2024-03-27 09:44:08 +0800

    * Working with game UI

--- features/game/src/main/java/io/github/japskiddin/sudoku/feature/main/GameMainFeatureUI.kt
+++ features/game/src/main/java/io/github/japskiddin/sudoku/feature/main/GameMainFeatureUI.kt
@@ -1,6 +1,21 @@
 package io.github.japskiddin.sudoku.feature.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -10,5 +25,89 @@ fun GameScreen() {
 
 @Composable
 internal fun GameScreen(viewModel: GameViewModel) {
+    Board(
+        values = arrayOf(
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+        )
+    )
+}
 
+@Composable
+internal fun Board(
+    values: Array<Int>,
+) {
+    val size = 9
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(width = 1.dp, color = Color.Black)
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            for (i in 1..size) {
+                Row {
+                    for (j in 1..size) {
+                        val index = (j * i) - 1
+                        Cell(value = values[index], modifier = Modifier
+                            .aspectRatio(1f)
+                            .weight(1f))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+internal fun Cell(
+    value: Int,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(color = Color.White)
+            .border(width = 1.dp, color = Color.Black)
+            .then(modifier),
+    ) {
+        Text(
+            text = value.toString(),
+            modifier = Modifier.padding(4.dp),
+        )
+    }
+}
+
+@Preview(
+    name = "Game Board",
+)
+@Composable
+fun BoardPreview() {
+    Board(
+        values = arrayOf(
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+        )
+    )
+}
+
+@Preview(
+    name = "Game Cell",
+)
+@Composable
+fun CellPreview() {
+    Cell(value = 1)
 }
