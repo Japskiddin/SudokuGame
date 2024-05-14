a8e33d902b6a0ac4f36f52f47d576d4fe823dc41
Nikita <japskiddin@gmail.com>
2024-05-14 19:46:30 +0800

    * Renamed package

--- database/src/main/java/io/github/japskiddin/sudoku/database/GameRoomDatabase.kt
+++ database/src/main/java/io/github/japskiddin/sudoku/database/GameRoomDatabase.kt
@@ -6,25 +6,25 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import io.github.japskiddin.sudoku.database.dao.HistoryDao
-import io.github.japskiddin.sudoku.database.models.HistoryDBO
+import io.github.japskiddin.sudoku.database.model.HistoryDBO
 import io.github.japskiddin.sudoku.database.utils.Converters
 
 class GameDatabase internal constructor(private val database: GameRoomDatabase) {
-    val historyDao: HistoryDao
-        get() = database.historyDao()
+  val historyDao: HistoryDao
+    get() = database.historyDao()
 }
 
 @Database(entities = [HistoryDBO::class], version = 1)
 @TypeConverters(Converters::class)
 internal abstract class GameRoomDatabase : RoomDatabase() {
-    abstract fun historyDao(): HistoryDao
+  abstract fun historyDao(): HistoryDao
 }
 
 fun GameDatabase(applicationContext: Context): GameDatabase {
-    val gameRoomDatabase = Room.databaseBuilder(
-        checkNotNull(applicationContext.applicationContext),
-        GameRoomDatabase::class.java,
-        "game"
-    ).build()
-    return GameDatabase(gameRoomDatabase)
+  val gameRoomDatabase = Room.databaseBuilder(
+    checkNotNull(applicationContext.applicationContext),
+    GameRoomDatabase::class.java,
+    "game"
+  ).build()
+  return GameDatabase(gameRoomDatabase)
 }
--- database/src/main/java/io/github/japskiddin/sudoku/database/dao/HistoryDao.kt
+++ database/src/main/java/io/github/japskiddin/sudoku/database/dao/HistoryDao.kt
@@ -4,23 +4,23 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import io.github.japskiddin.sudoku.database.models.HistoryDBO
+import io.github.japskiddin.sudoku.database.model.HistoryDBO
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface HistoryDao {
-    @Query("SELECT * FROM history")
-    fun getAll(): Flow<List<HistoryDBO>>
+  @Query("SELECT * FROM history")
+  fun getAll(): Flow<List<HistoryDBO>>
 
-    @Insert
-    suspend fun insert(histories: List<HistoryDBO>)
+  @Insert
+  suspend fun insert(histories: List<HistoryDBO>)
 
-    @Insert
-    suspend fun insert(history: HistoryDBO)
+  @Insert
+  suspend fun insert(history: HistoryDBO)
 
-    @Delete
-    suspend fun remove(history: HistoryDBO)
+  @Delete
+  suspend fun remove(history: HistoryDBO)
 
-    @Query("DELETE FROM history")
-    suspend fun clean()
+  @Query("DELETE FROM history")
+  suspend fun clean()
 }
