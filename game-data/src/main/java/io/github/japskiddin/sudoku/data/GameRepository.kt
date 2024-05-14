348951b82b7840cecdb9b99936235d743f0fce0f
Nikita <japskiddin@gmail.com>
2024-05-14 22:22:36 +0800

    * Updated datastore

--- game-data/src/main/java/io/github/japskiddin/sudoku/data/GameRepository.kt
+++ game-data/src/main/java/io/github/japskiddin/sudoku/data/GameRepository.kt
@@ -1,18 +1,11 @@
 package io.github.japskiddin.sudoku.data
 
-import io.github.japskiddin.sudoku.data.models.GameLevel
-import io.github.japskiddin.sudoku.data.utils.toGameLevel
 import io.github.japskiddin.sudoku.database.SudokuDatabase
 import io.github.japskiddin.sudoku.datastore.SettingsDatastore
 import jakarta.inject.Inject
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class GameRepository @Inject constructor(
   private val database: SudokuDatabase,
   private val datastore: SettingsDatastore
 ) {
-  fun getGameLevel(): Flow<GameLevel> {
-    return datastore.gameLevelFlow.map { it.toGameLevel() }
-  }
 }
--- game-data/src/main/java/io/github/japskiddin/sudoku/data/utils/Mappers.kt
+++ game-data/src/main/java/io/github/japskiddin/sudoku/data/utils/Mappers.kt
@@ -1,73 +1,49 @@
 package io.github.japskiddin.sudoku.data.utils
 
 import io.github.japskiddin.sudoku.data.models.Difficulty
-import io.github.japskiddin.sudoku.data.models.GameLevel
-import io.github.japskiddin.sudoku.datastore.models.GameLevelDO
 
 internal const val BOARD_SEPARATOR = ";"
 
-internal fun GameLevel.toGameLevelDO(): GameLevelDO {
-    return GameLevelDO(
-        this.playtime,
-        this.defaultBoard.toBoardString(),
-        this.currentBoard.toBoardString(),
-        this.completedBoard.toBoardString(),
-        this.actions,
-        this.difficulty.toInt()
-    )
-}
-
-internal fun GameLevelDO.toGameLevel(): GameLevel {
-    return GameLevel(
-        this.playtime,
-        this.defaultBoard.toIntArray(),
-        this.currentBoard.toIntArray(),
-        this.completedBoard.toIntArray(),
-        this.actions,
-        this.difficulty.toDifficulty(),
-    )
-}
-
 internal fun Int.toDifficulty(): Difficulty {
-    return when (this) {
-        1 -> Difficulty.EASY
-        2 -> Difficulty.NORMAL
-        3 -> Difficulty.HARD
-        4 -> Difficulty.EXPERT
-        else -> Difficulty.NORMAL
-    }
+  return when (this) {
+    1 -> Difficulty.EASY
+    2 -> Difficulty.NORMAL
+    3 -> Difficulty.HARD
+    4 -> Difficulty.EXPERT
+    else -> Difficulty.NORMAL
+  }
 }
 
 internal fun Difficulty.toInt(): Int {
-    return when (this) {
-        Difficulty.EASY -> 1
-        Difficulty.NORMAL -> 2
-        Difficulty.HARD -> 3
-        Difficulty.EXPERT -> 4
-    }
+  return when (this) {
+    Difficulty.EASY -> 1
+    Difficulty.NORMAL -> 2
+    Difficulty.HARD -> 3
+    Difficulty.EXPERT -> 4
+  }
 }
 
 internal fun String.toIntArray(): Array<IntArray> {
-    val items = split(BOARD_SEPARATOR)
-    val size = items.size
-    val array = Array(size) { IntArray(size) }
-    items.forEachIndexed { i, item ->
-        array[i] = item
-            .removeSurrounding("[", "]")
-            .split(",")
-            .map { it.toInt() }
-            .toIntArray()
-    }
-    return array
+  val items = split(BOARD_SEPARATOR)
+  val size = items.size
+  val array = Array(size) { IntArray(size) }
+  items.forEachIndexed { i, item ->
+    array[i] = item
+      .removeSurrounding("[", "]")
+      .split(",")
+      .map { it.toInt() }
+      .toIntArray()
+  }
+  return array
 }
 
 internal fun Array<IntArray>.toBoardString(): String {
-    val sb = StringBuilder()
-    for (i in indices) {
-        sb.append(this[i].contentToString())
-        if (i + 1 < size) {
-            sb.append(BOARD_SEPARATOR)
-        }
+  val sb = StringBuilder()
+  for (i in indices) {
+    sb.append(this[i].contentToString())
+    if (i + 1 < size) {
+      sb.append(BOARD_SEPARATOR)
     }
-    return sb.toString()
+  }
+  return sb.toString()
 }
