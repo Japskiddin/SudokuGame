a79c610e0e078cb5dd0620450b674b4d728f41ae
Nikita <japskiddin@gmail.com>
2024-05-29 22:02:39 +0800

    * Updated loading screen design

--- core/ui/src/main/java/io/github/japskiddin/sudoku/core/ui/component/Loading.kt
+++ core/ui/src/main/java/io/github/japskiddin/sudoku/core/ui/component/Loading.kt
@@ -4,10 +4,10 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +20,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import io.github.japskiddin.sudoku.core.ui.R
 import io.github.japskiddin.sudoku.core.ui.theme.OnPrimary
 import io.github.japskiddin.sudoku.core.ui.theme.Primary
 
@@ -80,20 +79,20 @@ internal fun LoadingContent(
         )
         .padding(16.dp)
     ) {
-      CircularProgressIndicator(
-        trackColor = Primary.copy(alpha = 0.2f),
-        color = Primary,
-        strokeWidth = 6.dp,
-        strokeCap = StrokeCap.Round,
-        modifier = Modifier.size(48.dp)
-      )
       Text(
         text = text,
         fontSize = 24.sp,
         fontWeight = FontWeight.Bold,
         color = Primary,
         textAlign = TextAlign.Center,
-        modifier = Modifier.padding(top = 16.dp)
+      )
+      LinearProgressIndicator(
+        trackColor = Primary.copy(alpha = .2f),
+        color = Primary,
+        strokeCap = StrokeCap.Round,
+        modifier = Modifier
+          .padding(top = 16.dp)
+          .height(8.dp),
       )
     }
   }
@@ -105,6 +104,6 @@ internal fun LoadingContent(
 @Composable
 internal fun LoadingPreview() {
   Loading(
-    text = stringResource(R.string.please_wait)
+    text = "Please, wait..."
   )
 }
