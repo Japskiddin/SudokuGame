50f6a7e3cd3340b858a19c2b83859f45029e701f
Nikita <japskiddin@gmail.com>
2024-05-30 21:37:45 +0800

    * Cleared dependencies in features:home:domain

--- features/home/domain/build.gradle.kts
+++ features/home/domain/build.gradle.kts
@@ -3,9 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 plugins {
   alias(libs.plugins.android.library)
   alias(libs.plugins.jetbrains.kotlin.android)
-  alias(libs.plugins.jetbrains.compose.compiler)
-  alias(libs.plugins.google.ksp)
   alias(libs.plugins.dagger.hilt.android)
+  alias(libs.plugins.google.ksp)
 }
 
 kotlin {
@@ -18,7 +17,6 @@ android {
 
   defaultConfig {
     minSdk = libs.versions.androidSdk.min.get().toInt()
-
     consumerProguardFiles("consumer-rules.pro")
   }
 
@@ -32,53 +30,24 @@ android {
   }
 
   buildFeatures {
-    compose = true
     buildConfig = true
-  }
-
-  kotlinOptions {
-    allWarningsAsErrors = false
-    freeCompilerArgs += listOf(
-      "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
-    )
   }
 }
 
 dependencies {
-  val composeBom = platform(libs.androidx.compose.bom)
-  implementation(composeBom)
-  androidTestImplementation(composeBom)
-
   implementation(libs.androidx.core.ktx)
   implementation(libs.javax.inject)
 
   implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.lifecycle.livedata.ktx)
-  implementation(libs.androidx.lifecycle.viewmodel.compose)
   implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
-  implementation(libs.androidx.compose.ui)
-  implementation(libs.androidx.compose.ui.unit)
-  implementation(libs.androidx.compose.ui.graphics)
-  implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material3)
-  implementation(libs.androidx.compose.runtime.livedata)
+  implementation(libs.jetbrains.kotlinx.coroutines.android)
 
-  implementation(libs.dagger.hilt.navigation.compose)
   implementation(libs.dagger.hilt.android)
   ksp(libs.dagger.hilt.compiler)
 
   implementation(projects.core.common)
-  implementation(projects.core.game)
-  implementation(projects.core.ui)
-  implementation(projects.gameData)
-  api(projects.navigation)
-
-  debugImplementation(libs.androidx.compose.ui.tooling)
-  debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-  testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  androidTestImplementation(libs.androidx.test.espresso.core)
-  androidTestImplementation(libs.androidx.test.ext.junit)
+  api(projects.core.game)
+  api(projects.gameData)
+  implementation(projects.navigation)
 }
--- features/home/domain/src/main/java/io/github/japskiddin/sudoku/feature/home/domain/UiState.kt
+++ features/home/domain/src/main/java/io/github/japskiddin/sudoku/feature/home/domain/UiState.kt
@@ -1,15 +1,10 @@
 package io.github.japskiddin.sudoku.feature.home.domain
 
 import androidx.annotation.StringRes
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.Stable
 
-@Stable
 public sealed class UiState {
-  @Stable
   public class Loading(@StringRes public val message: Int) : UiState()
 
-  @Immutable
   public data object Menu : UiState()
 
   public companion object {
