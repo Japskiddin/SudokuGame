39609f70bedc97348d83dcb2c5762cb3410c52e4
Nikita <japskiddin@gmail.com>
2024-06-10 20:16:47 +0800

    * Moved generate sudoku logic to use case

--- features/home/domain/src/main/java/io/github/japskiddin/sudoku/feature/home/domain/HomeViewModel.kt
+++ features/home/domain/src/main/java/io/github/japskiddin/sudoku/feature/home/domain/HomeViewModel.kt
@@ -3,14 +3,10 @@ package io.github.japskiddin.sudoku.feature.home.domain
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.github.japskiddin.sudoku.core.game.model.BoardCell
-import io.github.japskiddin.sudoku.core.game.qqwing.GameDifficulty
-import io.github.japskiddin.sudoku.core.game.qqwing.GameType
-import io.github.japskiddin.sudoku.core.game.qqwing.QQWingController
-import io.github.japskiddin.sudoku.core.game.utils.SudokuParser
-import io.github.japskiddin.sudoku.data.model.Board
 import io.github.japskiddin.sudoku.feature.home.domain.UiState.Loading
 import io.github.japskiddin.sudoku.feature.home.domain.usecase.CreateBoardUseCase
+import io.github.japskiddin.sudoku.feature.home.domain.usecase.GenerateSudokuUseCase
+import io.github.japskiddin.sudoku.feature.home.domain.usecase.SudokuNotGenerated
 import io.github.japskiddin.sudoku.navigation.AppNavigator
 import io.github.japskiddin.sudoku.navigation.Destination
 import kotlinx.coroutines.Dispatchers
@@ -19,7 +15,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.util.Calendar
 import javax.inject.Inject
 import javax.inject.Provider
@@ -29,7 +24,8 @@ public class HomeViewModel
 @Inject
 internal constructor(
     private val appNavigator: AppNavigator,
-    private val createBoardUseCase: Provider<CreateBoardUseCase>
+    private val createBoardUseCase: Provider<CreateBoardUseCase>,
+    private val generateSudokuUseCase: Provider<GenerateSudokuUseCase>
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(UiState.Initial)
     public val uiState: StateFlow<UiState>
@@ -51,49 +47,18 @@ internal constructor(
     }
 
     private fun generateNewBoard() {
-        val selectedType = GameType.DEFAULT9X9
-        val selectedDifficulty = GameDifficulty.INTERMEDIATE
-
         viewModelScope.launch(Dispatchers.IO) {
             _uiState.update { Loading(message = R.string.preparing_game_please_wait) }
 
-            val puzzle =
-                List(selectedType.size) { row ->
-                    List(selectedType.size) { col -> BoardCell(row, col, 0) }
-                }
-            val solvedPuzzle =
-                List(selectedType.size) { row ->
-                    List(selectedType.size) { col -> BoardCell(row, col, 0) }
-                }
-            val qqWingController = QQWingController()
-            val generatedBoard =
-                qqWingController.generate(selectedType, selectedDifficulty) ?: return@launch
-            val solvedBoard = qqWingController.solve(generatedBoard, selectedType)
-
-            if (qqWingController.isImpossible || qqWingController.solutionCount != 1) {
+            val board = try {
+                generateSudokuUseCase.get().invoke()
+            } catch (ex: SudokuNotGenerated) {
                 // TODO сообщить об ошибке
                 return@launch
             }
 
-            for (i in 0 until selectedType.size) {
-                for (j in 0 until selectedType.size) {
-                    puzzle[i][j].value = generatedBoard[i * selectedType.size + j]
-                    solvedPuzzle[i][j].value = solvedBoard[i * selectedType.size + j]
-                }
-            }
-
-            withContext(Dispatchers.IO) {
-                val sudokuParser = SudokuParser()
-                val board =
-                    Board(
-                        initialBoard = sudokuParser.boardToString(puzzle),
-                        solvedBoard = sudokuParser.boardToString(solvedPuzzle),
-                        difficulty = selectedDifficulty,
-                        type = selectedType
-                    )
-                val insertedBoardUid = createBoardUseCase.get().invoke(board)
-                navigateToGame(insertedBoardUid)
-            }
+            val insertedBoardUid = createBoardUseCase.get().invoke(board)
+            navigateToGame(insertedBoardUid)
 
             _uiState.update { UiState.Menu }
         }
---
+++ features/home/domain/src/main/java/io/github/japskiddin/sudoku/feature/home/domain/usecase/GenerateSudokuUseCase.kt
@@ -0,0 +1,50 @@
+package io.github.japskiddin.sudoku.feature.home.domain.usecase
+
+import io.github.japskiddin.sudoku.core.game.model.BoardCell
+import io.github.japskiddin.sudoku.core.game.qqwing.GameDifficulty
+import io.github.japskiddin.sudoku.core.game.qqwing.GameType
+import io.github.japskiddin.sudoku.core.game.qqwing.QQWingController
+import io.github.japskiddin.sudoku.core.game.utils.SudokuParser
+import io.github.japskiddin.sudoku.data.model.Board
+import javax.inject.Inject
+
+internal class GenerateSudokuUseCase @Inject constructor() {
+    operator fun invoke(
+        type: GameType = GameType.DEFAULT9X9,
+        difficulty: GameDifficulty = GameDifficulty.INTERMEDIATE
+    ): Board {
+        val sudokuParser = SudokuParser()
+        val boardSize = type.size
+
+        val puzzle = List(boardSize) { row ->
+            List(boardSize) { col -> BoardCell(row, col, 0) }
+        }
+        val solvedPuzzle = List(boardSize) { row ->
+            List(boardSize) { col -> BoardCell(row, col, 0) }
+        }
+        val qqWingController = QQWingController()
+        val generatedBoard = qqWingController.generate(type, difficulty)
+            ?: throw SudokuNotGenerated("Error with generating sudoku")
+        val solvedBoard = qqWingController.solve(generatedBoard, type)
+
+        if (qqWingController.isImpossible || qqWingController.solutionCount != 1) {
+            throw SudokuNotGenerated("Error with generating sudoku")
+        }
+
+        for (i in 0 until boardSize) {
+            for (j in 0 until boardSize) {
+                puzzle[i][j].value = generatedBoard[i * boardSize + j]
+                solvedPuzzle[i][j].value = solvedBoard[i * boardSize + j]
+            }
+        }
+
+        return Board(
+            initialBoard = sudokuParser.boardToString(puzzle),
+            solvedBoard = sudokuParser.boardToString(solvedPuzzle),
+            difficulty = difficulty,
+            type = type
+        )
+    }
+}
+
+internal class SudokuNotGenerated(message: String) : Exception(message)
