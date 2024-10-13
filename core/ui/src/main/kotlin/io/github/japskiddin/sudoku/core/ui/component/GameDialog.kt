56c19dd235c06d1d04d208f4e23972248587b255
Nikita <japskiddin@gmail.com>
2024-10-13 18:05:12 +0800

    * Updated dialog size

--- core/ui/src/main/kotlin/io/github/japskiddin/sudoku/core/ui/component/GameDialog.kt
+++ core/ui/src/main/kotlin/io/github/japskiddin/sudoku/core/ui/component/GameDialog.kt
@@ -1,17 +1,12 @@
 package io.github.japskiddin.sudoku.core.ui.component
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import io.github.japskiddin.sudoku.core.designsystem.theme.SudokuTheme
 
 @Composable
@@ -19,27 +14,12 @@ public fun GameDialog(
     onDismiss: () -> Unit,
     content: @Composable () -> Unit
 ) {
-    Dialog(
-        onDismissRequest = onDismiss,
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false,
-            dismissOnBackPress = true,
-            dismissOnClickOutside = true
-        )
-    ) {
+    Dialog(onDismissRequest = onDismiss) {
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.dialogBackground(),
             contentAlignment = Alignment.Center
         ) {
-            Box(
-                modifier = Modifier
-                    .safeContentPadding()
-                    .padding(36.dp)
-                    .dialogBackground(),
-                contentAlignment = Alignment.Center
-            ) {
-                content()
-            }
+            content()
         }
     }
 }
