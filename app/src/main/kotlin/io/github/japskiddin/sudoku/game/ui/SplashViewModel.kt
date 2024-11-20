a8b7a1982e9bf65e515546beef052eca98dcc62d
Nikita <japskiddin@gmail.com>
2024-11-20 14:32:02 +0800

    * Minor fixes

--- app/src/main/kotlin/io/github/japskiddin/sudoku/game/ui/SplashViewModel.kt
+++ app/src/main/kotlin/io/github/japskiddin/sudoku/game/ui/SplashViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,12 +16,14 @@ internal class SplashViewModel
 @Inject
 constructor() : ViewModel() {
     private val _isShowSplashScreen = MutableStateFlow(true)
-    val isShowSplashScreen = _isShowSplashScreen.asStateFlow()
+
+    val isShowSplashScreen: StateFlow<Boolean>
+        get() = _isShowSplashScreen.asStateFlow()
 
     init {
         viewModelScope.launch {
             delay(SPLASH_SCREEN_DURATION)
-            _isShowSplashScreen.value = false
+            _isShowSplashScreen.update { false }
         }
     }
 
