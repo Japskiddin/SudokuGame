31f8c1e333e1170d42718f10d8939e4ba7800865
Nikita <japskiddin@gmail.com>
2024-12-15 18:15:20 +0800

    - Updated tests for Record

--- core/database/data/src/androidTest/kotlin/io/github/japskiddin/sudoku/database/RecordDaoTest.kt
+++ core/database/data/src/androidTest/kotlin/io/github/japskiddin/sudoku/database/RecordDaoTest.kt
@@ -8,7 +8,9 @@ import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import io.github.japskiddin.sudoku.database.dao.BoardDao
 import io.github.japskiddin.sudoku.database.dao.RecordDao
-import io.github.japskiddin.sudoku.database.utils.createBoards
+import io.github.japskiddin.sudoku.database.entities.RecordDBO
+import io.github.japskiddin.sudoku.database.utils.createDummyBoard
+import io.github.japskiddin.sudoku.database.utils.createDummyBoards
 import io.github.japskiddin.sudoku.database.utils.createDummyRecord
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -36,14 +38,6 @@ class RecordDaoTest {
             .build()
         recordDao = database.recordDao()
         boardDao = database.boardDao()
-        createTestBoards()
-    }
-
-    private fun createTestBoards() {
-        val boards = createBoards(1, 2, 3)
-        runTest {
-            boardDao.insert(boards)
-        }
     }
 
     @After
@@ -53,8 +47,10 @@ class RecordDaoTest {
 
     @Test
     fun insertRecord_returnsTrue() = runTest {
+        val boards = createDummyBoards()
         val record = createDummyRecord(1)
 
+        boardDao.insert(boards)
         recordDao.insert(record)
 
         val latch = CountDownLatch(1)
@@ -62,6 +58,90 @@ class RecordDaoTest {
             recordDao.getAll().collect {
                 assertThat(it).contains(record)
                 latch.countDown()
+            }
+        }
+        latch.await()
+        job.cancelAndJoin()
+    }
+
+    @Test
+    fun insertRecordsList_returnsTrue() = runTest {
+        val boards = createDummyBoards()
+        val records = listOf(
+            createDummyRecord(1),
+            createDummyRecord(2),
+            createDummyRecord(3)
+        )
+
+        boardDao.insert(boards)
+        recordDao.insert(records)
+
+        val latch = CountDownLatch(1)
+        val job = async(Dispatchers.IO) {
+            recordDao.getAll().collect {
+                assertThat(it).isEqualTo(records)
+                latch.countDown()
+            }
+        }
+        latch.await()
+        job.cancelAndJoin()
+    }
+
+    @Test
+    fun deleteRecord_returnsTrue() = runTest {
+        val boards = createDummyBoards()
+        val record = createDummyRecord(1)
+
+        boardDao.insert(boards)
+        recordDao.insert(record)
+        recordDao.delete(record)
+
+        val latch = CountDownLatch(1)
+        val job = async(Dispatchers.IO) {
+            recordDao.getAll().collect {
+                assertThat(it).doesNotContain(record)
+                latch.countDown()
+            }
+        }
+        latch.await()
+        job.cancelAndJoin()
+    }
+
+    @Test
+    fun getAllRecordsWithType_returnsTrue() = runTest {
+        val expectedDifficulty = 2
+        val expectedType = 1
+        val boards = listOf(
+            createDummyBoard(uid = 1, difficulty = 2, type = 1),
+            createDummyBoard(uid = 2, difficulty = 1, type = 2),
+            createDummyBoard(uid = 3, difficulty = 2, type = 1),
+        )
+        val records = listOf(
+            createDummyRecord(1),
+            createDummyRecord(2),
+            createDummyRecord(3)
+        )
+        val foundBoards: List<Long> = boards
+            .filter {
+                it.difficulty == expectedDifficulty && it.type == expectedType
+            }
+            .map {
+                it.uid
+            }
+        val expectedRecords: List<RecordDBO> = records.filter {
+            it.uid in foundBoards
+        }
+
+        boardDao.insert(boards)
+        recordDao.insert(records)
+
+        val latch = CountDownLatch(1)
+        val job = async(Dispatchers.IO) {
+            recordDao.getAll(
+                difficulty = expectedDifficulty,
+                type = expectedType,
+            ).collect { records ->
+                assertThat(records).isEqualTo(expectedRecords)
             }
         }
         latch.await()
