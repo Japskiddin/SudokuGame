ed368e530079bc792048ff59e5fb92a98af499f6
Nikita <japskiddin@gmail.com>
2026-02-13 17:56:18 +0800

    - Updated AGP to 9.0 - Updated Gradle - Updated dependencies

--- gradle/build-logic/convention/src/main/kotlin/io/github/japskiddin/android/core/buildlogic/BaseExtensions.kt
+++ gradle/build-logic/convention/src/main/kotlin/io/github/japskiddin/android/core/buildlogic/BaseExtensions.kt
@@ -5,19 +5,10 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.PluginManager
 import org.gradle.kotlin.dsl.the
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 fun Project.plugins(block: PluginManager.() -> Unit) {
     with(pluginManager) {
         block()
-    }
-}
-
-fun Project.kotlinJvmCompilerOptions(block: KotlinJvmCompilerOptions.() -> Unit) {
-    tasks.withType<KotlinJvmCompile>().configureEach {
-        compilerOptions(block)
     }
 }
 
